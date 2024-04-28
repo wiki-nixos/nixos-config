73ee9da1cd49047efac70a4507cdd89e89b68242
eternalblissed <3tnl@proton.me>
2024-04-28 11:50:49 +1000

    970-desktop install + nvidia

--- nixos/970-desktop/configuration.nix
+++ nixos/970-desktop/configuration.nix
@@ -47,6 +47,7 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
+      nvidia.acceptLicense = true;
     };
   }; 
 
@@ -131,13 +132,51 @@
     layout = "au";
     variant = "";
   };
-   enable = true;
-   #libinput.enable = true;
-   displayManager.lightdm.enable = true;
-	 desktopManager = {
+  displayManager.lightdm.enable = true;
+		desktopManager = {
 			cinnamon.enable = true;
-	 };
+		};
 		displayManager.defaultSession = "cinnamon";
+   enable = true;
+  };
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+   hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
   };
 
   # Enable CUPS to print documents.
@@ -146,8 +185,9 @@
   # Enable sound with pipewire.
   sound.enable = true;
   security.rtkit.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
   # modules/home-manager/shell.nix.old
   #programs.zsh.enable = true;
